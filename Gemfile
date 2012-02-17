a97a6acc4dc476ceb62c120ba083bc92b3b27a86
Sam Goldman <sam@smartlogicsolutions.com>
2012-02-16 23:41:22 -0500

    didn't need webmock

--- Gemfile
+++ Gemfile
@@ -3,4 +3,3 @@ source "https://rubygems.org"
 gem "gems"
 
 gem "rspec"
-gem "webmock"
--- spec/dep_spec.rb
+++ spec/dep_spec.rb
@@ -1,6 +1,5 @@
 require "dep"
 require "json"
-require "webmock/rspec"
 
 describe Dep do
   let(:dep) { Dep.new("gem_name") }
@@ -10,12 +9,8 @@ describe Dep do
      {"number" => "1"}]
   }
   
-  before do
-    stub_request(:get, "https://rubygems.org/api/v1/versions/gem_name.yaml").
-      to_return(:body => JSON.generate(versions_resource))
-  end
-
   it "should return all revisions for a dep" do
+    Gems.stub!(:versions).with("gem_name").and_return(versions_resource)
     dep.revs.should eq(%w{3 2 1})
   end
 end
