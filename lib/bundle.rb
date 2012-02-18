b88b9e12caf2509e26d5ab9fab8874259e6a5c16
Sam Goldman <sam@smartlogicsolutions.com>
2012-02-17 19:37:10 -0500

    bundle to gemfile

--- lib/bundle.rb
+++ lib/bundle.rb
@@ -1,7 +1,33 @@
 require "bundler"
 
 class Bundle
-  def self.deps
-    Bundler.definition.dependencies.map(&:name)
+  class << self
+    def deps
+      dependencies.map { |dep| Dep.new(dep.name) }
+    end
+
+    def rev(dep_name)
+      dependencies.find { |dep| dep.name == dep_name }.requirement
+    end
+
+    def pin(dep_name, rev)
+      dep = dependencies.find { |dep| dep.name == dep_name }
+      dep.requirement.instance_variable_set(:@requirements, [Gem::Version.new(rev)])
+      dep.instance_variable_set(:@requirement, nil)
+    end
+
+    def gemfile
+      ['source "https://rubygems.org"', *deps.map { |dep| %Q{gem "#{dep.name}", "#{dep.current_rev}"} }].join("\n")
+    end
+
+    private
+
+    def dependencies
+      definition.dependencies
+    end
+
+    def definition
+      Bundler.definition
+    end
   end
 end
--- spec/bundle_spec.rb
+++ spec/bundle_spec.rb
@@ -1,19 +1,27 @@
 require "bundle"
 require "fakefs/spec_helpers"
 
+class Dep; end
+
 describe Bundle do
   include FakeFS::SpecHelpers
+
+  let(:dep) { stub(:name => "activesupport", :current_rev => "1.0") }
 
   before do
     File.open("Gemfile", "w+") do |f|
       f.puts 'source "https://rubygems.org"'
-      f.puts 'gem "a"'
-      f.puts 'gem "b"'
-      f.puts 'gem "c"'
+      f.puts 'gem "activesupport"'
     end
+
+    Dep.stub!(:new).with("activesupport").and_return(dep)
   end
 
   it "should return a list of deps" do
-    Bundle.deps.should eq(["a", "b", "c"])
+    Bundle.deps.should eq([dep])
+  end
+
+  it "should output a gemfile" do
+    Bundle.gemfile.should eq(%{source "https://rubygems.org"\ngem "activesupport", "1.0"})
   end
 end
