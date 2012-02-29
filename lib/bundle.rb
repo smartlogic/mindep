d90c573f2557c4d36468dc0efc64dc0dd693d35a
Sam Goldman <sam@smartlogicsolutions.com>
2012-02-28 19:25:45 -0500

    Remove unused method Bundle#revs

--- lib/bundle.rb
+++ lib/bundle.rb
@@ -6,10 +6,6 @@ class Bundle
       dependencies.map { |dep| Dep.new(dep.name) }
     end
 
-    def rev(dep_name)
-      dependencies.find { |dep| dep.name == dep_name }.requirement
-    end
-
     def gemfile
       ['source "https://rubygems.org"', *deps.map { |dep| %Q{gem "#{dep.name}", "#{dep.current_rev}"} }].join("\n")
     end
